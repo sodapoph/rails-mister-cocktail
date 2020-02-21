9e3d1a9655d8e1a90cbf82d31665712fdfcfa167
Soraida Henne <sodapoph@gmail.com>
2020-02-21 12:01:53 +0900

    functionality works

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,8 +20,10 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
   resources :cocktails do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:show, :edit, :update, :destroy]
 end
